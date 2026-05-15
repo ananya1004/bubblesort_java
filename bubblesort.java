30b63319872a9d4cb36d25340a62b8a58360793d
ananya1004 <ananyaangadi593@gmail.com>
2026-05-15 05:40:45 +0000

    this is bubble sort program

---
+++ bubblesort.java
@@ -0,0 +1,45 @@
+public class BubbleSortExample {
+
+    public static void bubbleSort(int[] arr) {
+        int n = arr.length;
+        boolean swapped;
+
+        // Traverse through all array elements
+        for (int i = 0; i < n - 1; i++) {
+            swapped = false;
+
+            // Last i elements are already in place
+            for (int j = 0; j < n - i - 1; j++) {
+                // Swap if the element is greater than the next
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                    swapped = true;
+                }
+            }
+
+            // If no swapping happened, array is sorted
+            if (!swapped) break;
+        }
+    }
+
+    public static void printArray(int[] arr) {
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {5, 3, 8, 4, 2};
+
+        System.out.println("Original array:");
+        printArray(arr);
+
+        bubbleSort(arr);
+
+        System.out.println("Sorted array:");
+        printArray(arr);
+    }
+}
